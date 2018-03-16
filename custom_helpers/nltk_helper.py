a8f014ec764ce12c518a202a0e6a99043f82006c
Paulo Henrique <paulo.costa@viaconsulting.com.br>
2018-03-15 22:44:50 -0300

    Atualizacao do projeto

--- custom_helpers/nltk_helper.py
+++ custom_helpers/nltk_helper.py
@@ -1,5 +1,10 @@
 import nltk
 from nltk.corpus import stopwords
+import re
+
+
+def has_numbers(input):
+    return bool(re.search(r'\d', input))
 
 
 def tokenize_abstract(abstract):
@@ -9,10 +14,23 @@ def tokenize_abstract(abstract):
     :return: list of tokens
     """
     tokens = nltk.word_tokenize(abstract)
+    tagged = nltk.pos_tag(tokens)
     new_tokens = []
-    for token in tokens:
-        new_tokens.append(token.lower())
-    return new_tokens
+    # remove verbs
+    for tag in tagged:
+        if str(tag[1]) == 'VBG' or str(tag[1]) == 'VBN' or str(tag[1]) == 'VBD' or str(tag[1]) == 'VBZ':
+            break
+        new_tokens.append(tag[0])
+    new_tokens_2 = []
+    # lower words
+    for token in new_tokens:
+        new_tokens_2.append(token.lower())
+    #stem words
+    stemmer = nltk.PorterStemmer()
+    new_tokens_3 = []
+    for token in new_tokens_2:
+        new_tokens_3.append(stemmer.stem(token))
+    return new_tokens_3
 
 
 def cleanup_tokens(tokens):
@@ -22,7 +40,7 @@ def cleanup_tokens(tokens):
     :return: list of important tokens
     """
     stopset = set(stopwords.words('english'))
-    cleanup = [i for i in tokens if i not in stopset if len(i) > 5]
+    cleanup = [i for i in tokens if i not in stopset if len(i) > 4 if has_numbers(i) is False]
     return cleanup
 
 
